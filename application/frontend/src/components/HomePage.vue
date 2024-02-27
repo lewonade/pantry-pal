ff7f3fca8e3cdb9c5a584d5644f8408d9376ae0d
aarxnxx <aaron.bernhart@student.htldornbirn.at>
2024-02-27 12:12:00 +0100

    Finished Allergies, implemented in API Request

--- application/frontend/src/components/HomePage.vue
+++ application/frontend/src/components/HomePage.vue
@@ -84,10 +84,18 @@ export default {
 		toggleAllergies() {
 			this.showAllergies = !this.showAllergies;
 		},
-
+		
 		saveAllergies() {
 			console.log('Selected Allergies:', this.selectedAllergies);
-			//bing bong
+			axios.post('http://localhost:3000/allergies', {
+				text: this.selectedAllergies
+			})
+			.then(response => {
+				console.log(response.data);
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
 		},
 
 		onFileSelected(event) {
